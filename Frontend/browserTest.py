5db3d9385bbe6592673740d909855c98a407bfd4
Pratyush Singh <aries.pratyush@gmail.com>
2022-12-22 01:28:45 +0530

    web : UI test updated

--- Frontend/browserTest.py
+++ Frontend/browserTest.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 os.environ['PATH']+=r"C:/SeleniumDrivers"
@@ -12,7 +13,17 @@ driver=webdriver.Chrome()
 time.sleep(5)
 
 driver.get("http://localhost:3000")
-driver.find_element(By.CLASS_NAME,'MuiSvgIcon-root').click()
 
+
+buttons =driver.find_elements(By.CLASS_NAME,'sc-gJqSRm')
+
+for i in buttons:
+    if i.text=='REGISTER':
+        i.click()
+        time.sleep(2)
+    if i.text=='SIGNIN':
+        i.click()
+        time.sleep(2)
+    
 
 time.sleep(5)
